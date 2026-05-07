c7cacd16ff1dc3f853003457f9021754e51a28b8
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2026-05-07 11:31:48 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     email: {
-      type: String,
+      type: String, 
       required: true,
       unique: true,
     },
